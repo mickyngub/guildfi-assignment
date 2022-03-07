cad4623919801b0a332a7f682643866e9aee5c89
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-07 12:49:09 +0700

    feat: add etherjs / connect with metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import styled from "styled-components/macro";
 import Navbar from "components/Navbar";
 import Map from "components/Map";
 
 const App = () => {
+  const [address, setAddress] = useState<string>("");
+  useEffect(() => {
+    (async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      // console.log("provider", provider.getSigner());
+      // console.log("signer is", await signer.getAddress());
+      let signerAddress = await signer.getAddress();
+      setAddress(signerAddress);
+    })();
+  }, []);
   return (
     <Wrapper>
       <Navbar />
-      <Map />
+      {address && <Map />}
     </Wrapper>
   );
 };
