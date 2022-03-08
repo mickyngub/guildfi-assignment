99326bece7edb483e2b24d360bdf503e9481301e
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-03-08 14:31:40 +0700

    feat: add cities icon

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -151,59 +151,78 @@ const OverlayPlanes = () => {
     shadowIsles,
     shurima,
     targon,
+    iconBilgewater,
+    iconDemacia,
+    iconFreljord,
+    iconIonia,
+    iconIxtal,
+    iconNoxus,
+    iconPiltoverZaun,
+    iconShadowIsles,
+    iconShurima,
+    iconTargon,
   } = useLoadTexture();
   return (
     <>
       <OverlayPlane
         map={ionia}
+        icon={iconIonia}
         boxPosition={[1.75, 0.7, 0.1]}
         renderOrder={1}
         transparent={true}
       />
       <OverlayPlane
         map={demacia}
+        icon={iconDemacia}
         boxPosition={[-1.4, 0.18, 0.05]}
         renderOrder={1}
         transparent={true}
       />
       <OverlayPlane
         map={bilgewater}
+        icon={iconBilgewater}
         boxPosition={[1.8, -0.6, 0.1]}
         renderOrder={1}
         transparent={true}
       />
       <OverlayPlane
         map={shadowIsles}
+        icon={iconShadowIsles}
         boxPosition={[2.3, -1.3, 0.1]}
         renderOrder={1}
         transparent={true}
       />
       <OverlayPlane
         map={freljord}
+        icon={iconFreljord}
         boxPosition={[-1.2, 0.9, 0.05]}
         renderOrder={1}
         transparent={true}
       />
       <OverlayPlane
         map={ixtal}
+        icon={iconIxtal}
         boxPosition={[0.9, -1.1, 0.1]}
         renderOrder={1}
         transparent={true}
       />
       <OverlayPlane
         map={noxus}
+        icon={iconNoxus}
         boxPosition={[-0, 0.55, 0.1]}
         renderOrder={1}
         transparent={true}
       />
       <OverlayPlane
         map={shurima}
+        icon={iconShurima}
         boxPosition={[0.2, -1.15, 0.1]}
         renderOrder={1}
         transparent={true}
       />
       <OverlayPlane
         map={targon}
+        icon={iconTargon}
         boxPosition={[-0.75, -1.25, 0.1]}
         renderOrder={1}
         transparent={true}
@@ -217,6 +236,7 @@ const OverlayPlane = ({
   renderOrder = 0,
   transparent = false,
   boxPosition = [0, 0, 0.2],
+  icon,
   ...props
 }: any) => {
   const currentRef = useRef<THREE.Mesh>(null!);
@@ -231,8 +251,8 @@ const OverlayPlane = ({
           currentRef.current.position.z = -0.1;
         }}
       >
-        <boxGeometry args={[0.1, 0.1, 0.1]} />
-        <meshStandardMaterial color="red" />
+        <circleGeometry args={[0.1, 32]} />
+        <meshStandardMaterial map={icon} />
       </mesh>
       <mesh ref={currentRef} position={[0, 0, -0.1]} renderOrder={renderOrder}>
         <planeBufferGeometry args={[6.8, 6.8, 64, 64]} />
--- src/hooks/useLoadTexture.ts
+++ src/hooks/useLoadTexture.ts
@@ -14,6 +14,16 @@ const useLoadTexture = () => {
     noxus,
     shurima,
     targon,
+    iconBilgewater,
+    iconDemacia,
+    iconFreljord,
+    iconIonia,
+    iconIxtal,
+    iconNoxus,
+    iconPiltoverZaun,
+    iconShadowIsles,
+    iconShurima,
+    iconTargon,
   ] = useLoader(TextureLoader, [
     "terrain_z1.jpg",
     "depth_z1.jpg",
@@ -26,6 +36,16 @@ const useLoadTexture = () => {
     "overlay_noxus.png",
     "overlay_shurima.png",
     "overlay_targon.png",
+    "icon_bilgewater.png",
+    "icon_demacia.png",
+    "icon_freljord.png",
+    "icon_ionia.png",
+    "icon_ixtal.png",
+    "icon_noxus.png",
+    "icon_piltover-zaun.png",
+    "icon_shadow-isles.png",
+    "icon_shurima.png",
+    "icon_targon.png",
   ]);
 
   return {
@@ -40,6 +60,16 @@ const useLoadTexture = () => {
     noxus,
     shurima,
     targon,
+    iconBilgewater,
+    iconDemacia,
+    iconFreljord,
+    iconIonia,
+    iconIxtal,
+    iconNoxus,
+    iconPiltoverZaun,
+    iconShadowIsles,
+    iconShurima,
+    iconTargon,
   };
 };
 
